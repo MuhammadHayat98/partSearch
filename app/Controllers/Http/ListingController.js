d975565eae42d1ed23464a87f02fb99f06daab79
MuhammadHayat98 <muhammad.hayat.698@my.csun.edu>
2019-12-17 10:17:02 -0800

    Item search

--- app/Controllers/Http/ListingController.js
+++ app/Controllers/Http/ListingController.js
@@ -1,46 +1,37 @@
 'use strict'
-const GSR = require('google-search-results-nodejs')
-const https = require('https');
 const request = require('request');
+var sortJsonArray = require('sort-json-array');
 class ListingController {
     async view ({request,view}){
-        let client = new GSR.GoogleSearchResults("99ecb6efc243f1e5d8cb3c5ed3fe736a412bfdaaf2158122d72f911a56dc544f")
-
-        var parameter = {
-            engine: "google",
-            q: "xbox",
-            google_domain: "google.com",
-            gl: "us",
-            hl: "en",
-            tbm: "shop",
-            num: "1"
-        };
-
-        var callback = function(data) {
-            //console.log(data)
-            return data
-            //console.log(res)
+        try {
+            const items = await this.getJson(request.input('search'))
+            console.log(items)
+            return view.render('listing', { items: items.data })
+          } catch (e) {
+            console.log(e.message)
+            return view.render('listing', { books: [] })
+          }
         
-            //console.log(res)
-        }
-
-        // Show result as JSON
-
-        // console.log(client.json(parameter, callback));
-        var res = this.getResults("t")
-        console.log(res)
-        return view.render('listing', {items: res});
     }
 
-    getResults(itemName) {
-        // var res = "t";
-        // request('https://serpapi.com/search.json?engine=google&q=tv&google_domain=google.com&gl=us&hl=en&num=10&tbm=shop&api_key=99ecb6efc243f1e5d8cb3c5ed3fe736a412bfdaaf2158122d72f911a56dc544f', { json: true }, (err, res, body) => {
-        // if (err) { return console.log(err); }
-        // res = body.shopping_results[0]
-        // }).end();
-        // return res;
-       
-    
+    getJson(itemName) {
+        return new Promise((resolve, reject) => {
+            request.get({ url: `https://serpapi.com/search.json?engine=google&q=${itemName}&location=United+States&google_domain=google.com&gl=us&hl=en&num=8&tbm=shop&api_key=99ecb6efc243f1e5d8cb3c5ed3fe736a412bfdaaf2158122d72f911a56dc544f` },
+              (err, request, body) => {
+                if (err) { return reject(err) }
+      
+                if (request.statusCode === 403) {
+                  return reject(new Error('Invalid New York Times API Key'))
+                }
+      
+                return resolve({
+                  data : sortJsonArray(JSON.parse(body).shopping_results, "extracted_price")
+                })
+              })
+          })
+
+
+    }
 }
 
 module.exports = ListingController
