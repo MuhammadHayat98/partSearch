406012aa6333403fa5be23231c06a7f5d3506e91
herreraosmar83 <osmar.herreraperez.945@my.csun.edu>
2019-12-17 19:25:45 -0800

    bugs atttempted auth

--- app/Controllers/Http/RegisterController.js
+++ app/Controllers/Http/RegisterController.js
@@ -1,8 +1,21 @@
 'use strict'
+const User = User('App/Models/User')
 
 class RegisterController {
   async view({view}){
     return view.render('register')
+  }
+
+  async store({request,response}){
+    console.log('test')
+    // const user = await User.view({
+    //   username: request.input('usernamesignup'),
+    //   email: request.input('emailsignup'),
+    //   password: request.input('passwordsignup'),
+    //   password_confirm: request.input('passwordsignup_confirm'),
+    // })
+    // sessionStorage.flash({successMessage:'yes'})
+    // return response.redirect('back')
   }
 }
 
