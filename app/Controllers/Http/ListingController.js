a27b90118f7b9ca04c1f8e17f8d19b0ad3da89d9
MuhammadHayat98 <muhammad.hayat.698@my.csun.edu>
2019-12-22 01:26:50 -0800

    secured api key

--- app/Controllers/Http/ListingController.js
+++ app/Controllers/Http/ListingController.js
@@ -1,6 +1,8 @@
 'use strict'
 const request = require('request');
 var sortJsonArray = require('sort-json-array');
+const Env = use('Env')
+const key = Env.get('API_KEY')
 class ListingController {
     async view ({request,view}){
         try {
@@ -16,7 +18,7 @@ class ListingController {
 
     getJson(itemName) {
         return new Promise((resolve, reject) => {
-            request.get({ url: `https://serpapi.com/search.json?engine=google&q=${itemName}&location=United+States&google_domain=google.com&gl=us&hl=en&num=40&tbm=shop&api_key=99ecb6efc243f1e5d8cb3c5ed3fe736a412bfdaaf2158122d72f911a56dc544f` },
+            request.get({ url: `https://serpapi.com/search.json?engine=google&q=${itemName}&location=United+States&google_domain=google.com&gl=us&hl=en&num=40&tbm=shop&api_key=${key}` },
               (err, request, body) => {
                 if (err) { return reject(err) }
       
